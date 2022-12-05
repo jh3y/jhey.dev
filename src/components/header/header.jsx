667f16d9c15fc36426f3e1433515b3dacd674359
jh3y <jh3y@users.noreply.github.com>
2022-12-05 02:47:21 +0000

    feat: start scaffolding out a feed UI

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 const LayoutHeader = ({ children }) => {
   return (
-    <header className="max-w-[65ch] mv-0 mx-auto">
-      <h1>Jhey Tompkins</h1>
-      <h2>Brings ideas to life with code.</h2>
-      <p>
-        Web Developer. Google DevRel. Content Creator. Whimsical Specialist.
-        International Speaker.
-      </p>
+    <header className="w-main-content max-w-full mv-0 mx-auto">
+      <div className="grid grid-cols-[auto_1fr] gap-fluid-space-0 justify-start items-center">
+        <img className="rounded-full aspect-square w-fluid-size-9 border-2 border-text-1" src="https://picsum.photos/300/300?random=23" alt="" /> 
+        <h1 className="leading-[0.9] text-fluid-3 sm:text-fluid-5">Jhey<br/>Tompkins<span role="img">😎</span></h1>
+      </div>
+      <div className="py-fluid-space-2">
+        <p>🤓 Brings ideas to life with code.</p>
+        <p>Whimsy Dev Supreme</p>
+        <p>International Speaker</p>
+        <p>Joined 2022</p>
+        <p>Null island</p>
+        <p>Google Dev Rel</p>
+        <p>Links</p>
+        <button className="bg-red-500">Follow</button>
+      </div>
     </header>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,26 +2,30 @@
 const plugin = require("tailwindcss/plugin");
 const defaultTheme = require("tailwindcss/defaultTheme");
 // Set up spacing extension from Utopia variables.
-const sizes = ["3xs", "2xs", "xs", "s", "m", "l", "xl", "2xl", "3xl", "4xl"];
+const sizes = ["3xs", "2xs", "xs", "s", "m", "l", "xl", "2xl", "3xl", "4xl", "5xl", "6xl"];
 const spacing = {};
 sizes.forEach((size, index) => {
-	spacing[`fluid-${size}`] = `var(--space-${size})`;
+	spacing[`fluid-size-${index - 2}`] = `var(--step-${index-2})`;
+	spacing[`fluid-space-${index - 2}`] = `var(--space-${size})`;
 	if (size !== sizes.at(sizes.length - 1))
 		spacing[
-			`fluid-${size}-${sizes.at(index + 1)}`
+			`fluid-space-${index-2}-${index-1}`
 		] = `var(--space-${size}-${sizes.at(index + 1)})`;
 });
 
 // Set up typography scales
 const fontSize = {};
-for (let i = -2; i < 9; i++) {
-	fontSize[i] = `var(--step-${i})`;
+for (let i = -2; i < 10; i++) {
+	fontSize[`fluid-${i}`] = `var(--step-${i})`;
 }
 
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
+			width: {
+				'main-content': '60ch',
+			},
 			spacing,
 			fontSize,
 			fontFamily: {
