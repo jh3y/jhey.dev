633dda98762fe22eceff2e1c3ee96952384f2899
jh3y <jh3y@users.noreply.github.com>
2022-12-29 12:21:41 -0800

    feat: deploy pagination

--- src/components/feed-layout/feed-layout.jsx
+++ src/components/feed-layout/feed-layout.jsx
@@ -8,10 +8,10 @@ const FeedLayout = ({ posts, category = '', currentPage, totalPages, route = "/p
   return (
     <>
       <LayoutHeader />
-      <main class="w-main-content max-w-full mv-0 mx-auto grid justify-center pt-8">
+      <main className="w-main-content max-w-full mv-0 mx-auto grid justify-center pt-8">
         {posts.length === 0 && (
           <p
-            class="border-2 border-current text-center text-text-2 p-4 grid items-center rounded-md"
+            className="border-2 border-current text-center text-text-2 p-4 grid items-center rounded-md"
           >
             {`No ${category} cheeps to show currently.`}
           </p>
--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const Pagination = ({ current, total, prefix = '/posts', splice = 5 }) => {
   if (total <= 1 || !current || !total) return null
   return (
-    <nav aria-labelledby="pagination-header" class="pagination">
-      <h2 class="sr-only" id="pagination-header">
+    <nav aria-labelledby="pagination-header" className="py-8">
+      <h2 className="sr-only" id="pagination-header">
         Pagination links
       </h2>
       <ul className="flex items-center justify-center">
-        <li class="previous">
+        <li>
           {current > 1 && (
             <a
               href={`${prefix}/${current - 1}`}
@@ -22,9 +22,9 @@ const Pagination = ({ current, total, prefix = '/posts', splice = 5 }) => {
                 class="w-6 h-6"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </a>
@@ -39,9 +39,9 @@ const Pagination = ({ current, total, prefix = '/posts', splice = 5 }) => {
                 class="w-6 h-6"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </span>
@@ -82,9 +82,9 @@ const Pagination = ({ current, total, prefix = '/posts', splice = 5 }) => {
                 class="w-6 h-6"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </a>
@@ -99,9 +99,9 @@ const Pagination = ({ current, total, prefix = '/posts', splice = 5 }) => {
                 class="w-6 h-6"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>
             </span>
