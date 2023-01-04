f355d4d199a4177711515f05f91e706b2c4f3ca7
jh3y <jh3y@users.noreply.github.com>
2023-01-03 23:26:27 -0800

    feat: RSS content changes

--- src/components/post-header/post-header.jsx
+++ src/components/post-header/post-header.jsx
@@ -99,7 +99,7 @@ const LayoutHeader = ({ character, ...props }) => {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
-          <span>{`${readingTime.readingTime} min`}</span>
+          <span>{`~${readingTime.readingTime} min`}</span>
         </span>
         <span className="flex items-center gap-1">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
--- src/pages/rss/_htmlGenerator.js
+++ src/pages/rss/_htmlGenerator.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import ReactDom from 'react-dom/server'
-import sanitizeHtml from 'sanitize-html';
+import sanitizeHtml from 'sanitize-html'
 import ContentBlock from '../../components/content-block/content-block.jsx'
 
-const genHtml = children => {
+const allowedTags = [
+  ...sanitizeHtml.defaults.allowedTags,
+  'img',
+  'hr',
+  'audio',
+  'video',
+  'blockquote',
+]
+
+const allowedAttributes = {
+  ...sanitizeHtml.defaults.allowedAttributes,
+  audio: ['src', 'controls'],
+}
+
+const genHtml = (children) => {
   const content = ReactDom.renderToStaticMarkup(
     React.createElement(ContentBlock, {
       children,
-      type: 'rss'
+      type: 'rss',
     })
   )
-  return sanitizeHtml(content)
+
+  return sanitizeHtml(content, {
+    allowedTags,
+    allowedAttributes,
+  })
 }
 
 export default genHtml
